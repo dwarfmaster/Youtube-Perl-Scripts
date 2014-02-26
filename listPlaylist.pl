a0b81b58f8653ef39dd5582417888de40d488688
lucas8 <luc.linux@mailoo.org>
2014-02-26 09:51:57 +0100

    Improved listPlaylist.pl.

--- listPlaylist.pl
+++ listPlaylist.pl
@@ -4,33 +4,58 @@ use strict;
 use warnings;
 
 die "Usage : ./listPlaylist.pl [playlist path] [url]" if scalar(@ARGV) != 2;
-my ($path, $url) = @ARGV;
-my $firsturl = $url;
+my ($path, $firsturl) = @ARGV;
 
 my $fd;
 open $fd, ">$path" or die "Couldn't open $path.";
 print $fd "#EXTM3U\n\n";
 
-$url = nextItem($url);
-parse($url, $fd);
-while($url ne $firsturl) {
-    $url = nextItem($url);
+my @urls = listItems($firsturl);
+foreach my $url (@urls) {
     parse($url, $fd);
 }
 close $fd;
 
-sub nextItem {
+sub listItems {
     my ($url) = @_;
     my $html = `wget "$url" -O - 2> /dev/null`;
+    my @lines = split '\n', $html;
 
-    die "Invalid html syntax, may not be a Youtube List page." if not $html =~ /<a href="(.*)" class=".*next-playlist-list-item/g;
-    $url = "https://www.youtube.com$1";
-    print "Next url is \"$url\".\n";
+    my @urls;
+    for my $line (@lines) {
+        if($line =~ /<a.*class=".*playlist-video/
+                and $line =~ /<a.*href="([^"]*)"/) {
+            push @urls, "https://www.youtube.com$1";
+            print "Added url www.youtbe.com$1.\n";
+        }
+    }
+    print "\n";
 
-    return $url;
+    return @urls;
 }
 
 sub parse {
     my ($url,$fd) = @_;
+    print "Parsing $url.\n";
+    my $html   = `wget "$url" -O - 2> /dev/null`;
+    my $title  = getTitle($html);
+    my $stream = `youtube-dl -g "$url"`;
+
+    print $fd "#EXTINF:-1, $title\n$stream\n\n";
+}
+
+sub getTitle {
+    my ($html) = @_;
+    my $title;
+
+    if($html =~ /<title>(.*)<\/title>/) {
+        $title = $1;
+        $title =~ s/( )*-Youtube.*//;
+    }
+    else {
+        $title = "Unknwown title.";
+    }
+
+    return $title;
 }
 
